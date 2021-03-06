7b5e13a3428d7026ae8cfbd6689d6d6fecb8a96f
Roy Brez <rbrbrbrb8@gmail.com>
2021-03-06 23:21:34 +0200

    need to implement passport

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session');
+
+const MongoStore = require('connect-mongo').default;
 const app = express();
 const port = 3000;
 
-
+const sessionStore = MongoStore.create({
+  mongoUrl:'mongodb+srv://rbrbrbrb8:rbpromongorb23@clusterproject.pzpyd.mongodb.net/ProjectDatabase?retryWrites=true&w=majority',
+  collectionName:'Sessions'
+});
 
 const signUpRouter = require('./routes/signUpRoute');
 
@@ -18,14 +24,36 @@ db.once('open',function(){
       });
     
 });
+
+//init parsers
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+//setting up session
+app.use(session({
+  secret:'some secret',
+  resave:false,
+  saveUninitialized:true,
+  store:sessionStore,
+  cookie:{
+    maxAge:1000*60*60*24
+  }
+}));
+
 app.use(express.static('static'));
 
+//using routers
 app.use('/signup',signUpRouter);
 
+
+//needs changing to loginRouter
 app.get('/', (req, res) => {
+  if(req.session.viewCount){
+    req.session.viewCount++;
+  } else{
+    req.session.viewCount = 1;
+  }
+  console.log(`you have visited this page ${req.session.viewCount} times`);
   res.sendFile('/pages/index.html',{root:__dirname});
 });
 
--- handlers/db/dbHandler.js
+++ handlers/db/dbHandler.js
@@ -4,16 +4,15 @@ const dbHandler = {};
 
 function getModel(model)
 {
-  console.log()
+  
   return require('./models/' + model);
 }
 
 dbHandler.addDocumentToDb = (modelName,document) => {
   const Model = getModel(modelName);
-  console.log(String(Model));
   const newDoc = new Model(document);
-  db.collection(String(Model.prototype.collection)).insertOne(newDoc).then(resolve => {
-    console.log("successful insertion to" + String(Model.prototype.collection));
+  newDoc.save().then(savedDoc => {
+    console.log("saved successfully to db");
   });
 }
 
