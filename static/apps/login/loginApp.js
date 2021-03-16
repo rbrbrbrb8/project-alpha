31a496876ca787210b5ffa1af25c2ffb09055b56
moshecohen1 <moshecohen7575@gmail.com>
2021-03-16 18:07:59 +0200

    sign up prevention is working buggily, fix

--- static/apps/login/loginApp.js
+++ static/apps/login/loginApp.js
@@ -2,13 +2,10 @@ const loginApp = angular.module('LoginApp',['loginModule']);
 
 
 loginApp.controller('LoginController',['$scope','loginHttpMethods',function($scope,loginHttpMethods){
-    const validateUsername = function(username){
-        console.log(username);
-        return true;
-    };
-    const validatePassword = function(password){
-        console.log(password);
-        return true;
+    const validateDetails = function(username){
+        let usernameValidator = new RegExp("^(?![0-9]{6})[0-9a-zA-Z]{6,8}$");
+        let passwordValidator = new RegExp("^(?![0-9]{6})[0-9a-zA-Z]{8,12}$");
+        return usernameValidator.test(username) && passwordValidator.test(password);
     };
     $scope.msg_invalid_user = "Username must be 6-8 characters long, and contain only letters and numbers";
     $scope.msg_invalid_password = "Password must be 8-12 characters long, and contain only letters and numbers";
@@ -16,11 +13,12 @@ loginApp.controller('LoginController',['$scope','loginHttpMethods',function($sco
 
     $scope.checkCredentialsAndLogin = function(){
         console.log("got here");
-        if(validateUsername($scope.login.username) && validatePassword($scope.login.password)){
+        const isValid = validateDetails($scope.login.username,$scope.login.password);
+        if(!isValid){
             loginHttpMethods.requestLogin($scope.login.username,$scope.login.password)
             .then(res => {
                 console.log(res.data);
-                window.location.href = '/welcome.html';
+            
             })
         };
 
--- static/apps/signUp/signUpApp.js
+++ static/apps/signUp/signUpApp.js
@@ -4,26 +4,22 @@ const signUpApp = angular.module('SignUpApp',['signUpModule']);
 
 
 signUpApp.controller('SignUpController',['$scope','httpMethods',function($scope,httpMethods){
-    const validateUsername = function(username){
-        console.log(username);
-        return true;
-    };
-    const validatePassword = function(password){
-        console.log(password);
-        return true;
+    const validateDetails = function(username,password){
+        let usernameValidator = new RegExp("^(?![0-9]{6})[0-9a-zA-Z]{6,8}$");
+        let passwordValidator = new RegExp("^(?![0-9]{6})[0-9a-zA-Z]{8,12}$");
+        return usernameValidator.test(username) && passwordValidator.test(password);
     };
     $scope.msg_invalid_user = "Username must be 6-8 characters long, and contain only letters and numbers";
     $scope.msg_invalid_password = "Password must be 8-12 characters long, and contain only letters and numbers";
     $scope.signup = {};
 
-    $scope.addToDb = function(){
+    $scope.preventIfInvalid = function(event){
         console.log("got here");
-        if(validateUsername($scope.signup.username) && validatePassword($scope.signup.password)){
-            httpMethods.requestSignUp($scope.signup.username,$scope.signup.password)
-            .then( res => {
-                console.log(res);
-                // window.location.assign(res.url);
-            })
+        const isValid = validateDetails($scope.signup.username,$scope.signup.password);
+        if(!isValid){
+            event.preventDefault();
+            console.log("event prevented, invalid username/password");
+            alert("please enter valid username and password");
         };
 
     };
