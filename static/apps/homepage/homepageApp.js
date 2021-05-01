19a3c552631d5b96ef4732170f737f629b53fa02
Roy Brez <rbrbrbrb8@gmail.com>
2021-05-02 01:49:57 +0300

    progress in navbar

--- static/apps/homepage/homepageApp.js
+++ static/apps/homepage/homepageApp.js
@@ -5,3 +5,15 @@ homepageApp.controller('HomepageController',['$scope',function($scope){
   
 }]); 
 
+homepageApp.directive('navbar',[function(){
+    return{
+        restrict: 'E',
+        scope:{}, //add user info to scope so the navbar can load username and profile pic
+        controller: function($scope){
+
+        },
+        templateUrl:'views/navbar.html'
+
+
+    }
+}]);
