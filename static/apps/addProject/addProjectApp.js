bed694d72b6bd8c9ef6023c93108398d1aa61cca
moshecohen1 <moshecohen7575@gmail.com>
2021-05-06 17:48:23 +0300

    work

---
+++ static/apps/addProject/addProjectApp.js
@@ -0,0 +1,19 @@
+const addProjectApp = angular.module('AddProjectApp',['ngMaterial']);
+
+
+addProjectApp.controller('AddProjectController',['$scope',function($scope){
+  
+}]); 
+
+addProjectApp.directive('navbar',[function(){
+    return{
+        restrict: 'E',
+        scope:{}, //add user info to scope so the navbar can load username and profile pic
+        controller: function($scope){
+
+        },
+        templateUrl:'views/navbar.html'
+
+
+    }
+}]);
